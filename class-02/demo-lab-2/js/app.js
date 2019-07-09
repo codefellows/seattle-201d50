5f48e99fbf3c71b5f0b0bbd4d10b82e0a33d798b
LENA EIVY <applena@gmail.com>
2019-07-09 14:41:29 -0700

    day two demo updated

---
+++ class-02/demo-lab-2/js/app.js
@@ -0,0 +1,22 @@
+// if (some condition is met){
+//   then do something
+// } else {
+//   do something else
+// }
+
+var likesStrangerThings = prompt('Do you like Stranger Things?');
+likesStrangerThings = likesStrangerThings.toLowerCase();
+
+if(likesStrangerThings === 'yes'){
+  alert('I like it too!');
+} else {
+  alert('That sucks');
+}
+
+// write a yes/no prompt that uses the respone in an if/else conditional
+
+
+
+
+
+
