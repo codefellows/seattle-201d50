55136e602d1dc605493ad5e1f6695c6ed54d20b5
LENA EIVY <applena@gmail.com>
2019-07-12 17:34:51 -0700

    class 6 readme updated

--- class-05/lab-a/starterCode/app.js
+++ class-05/lab-a/starterCode/app.js
@@ -51,6 +51,8 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
+var sumOne = sum(a, b);
+var sumTwo = sum(sumOne, c);
 
 }
 
