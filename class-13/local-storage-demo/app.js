cdc66a2200210b0b8c66d0c9ab046f9f9059f774
LENA EIVY <applena@gmail.com>
2019-07-24 13:24:05 -0700

    local storage updated

--- class-13/local-storage-demo/app.js
+++ class-13/local-storage-demo/app.js
@@ -1,4 +1,33 @@
+'use strict';
 
+var allCats = [];
+
+function Cat(name, age, grumpy, cuddly){
+  this.name = name;
+  this.age = age;
+  this.grumpy = grumpy;
+  this.cuddly = cuddly;
+  this.fat = true;
+  this.fluffy = true;
+  this.hungry = true;
+  allCats.push(this);
+}
+
+new Cat('Adonis', 10, 'yes', false);
+new Cat('Larry', 12, 'sometimes', true);
+new Cat('Rogue', 5, 'never', true);
+
+///////make allCats JSON/////////
+var jsonCats = JSON.stringify(allCats);
+
+//////store in Local Storage//////////
+localStorage.setItem('cats', jsonCats);
+
+//////get out of local storage////////
+var storageCats = localStorage.getItem('cats');
+
+/////turn it back into my data/parse it!////
+var parsedCats = JSON.parse(storageCats);
 
 
 
